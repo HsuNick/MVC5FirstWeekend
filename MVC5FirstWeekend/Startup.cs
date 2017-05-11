463c219465bcf130306b66961499ae14c195cbfc
Hsu <boy28213@gmail.com>
2017-05-11 17:48:54 +0800

    加入專案檔案。

---
+++ MVC5FirstWeekend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5FirstWeekend.Startup))]
+namespace MVC5FirstWeekend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
